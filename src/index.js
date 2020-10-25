a081ed6ba0734a823b6921b7a74829d06aa8169b
sshagd <pagoda.sashka@gmail.com>
2020-10-25 11:14:09 +0300

    init: start Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var i,
+        l=str.length,
+        k,
+        char,
+        last,
+        stack=[];
+
+    for(i=0; i<l; i++){
+        char=str[i];
+        for(k = 0; k < bracketsConfig.length; k++){
+            if(char===bracketsConfig[k][0]){
+                stack.push(char);
+            }else if(char===bracketsConfig[k][1]){
+                if(stack.length>0){
+                    last=stack[stack.length-1];
+                    if ((char === bracketsConfig[k][1] && last === bracketsConfig[k][0])) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+    }
+    return stack.length===0;
+
+};
